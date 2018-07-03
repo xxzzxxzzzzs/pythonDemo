e506996fc224da0d12fb7ef5d52a934141951007
xxzzxxzzzzs <740866631@qq.com>
2018-07-03 23:42:49 +0800

    fq

--- TCP/UDP/udp.py
+++ TCP/UDP/udp.py
@@ -1,7 +1,9 @@
 import socket
+# 飞Q发送消息报文的格式
+mystr = "1_lbt4_10#32899#002481627512#0#0#0:1289671407:512312:你的hello:288:你好"
 
-import time
-
-udp=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-udp.sendto("12dasdasewqe3".encode("gbk"),("192.168.1.2", 2425))
+# socket.AF_INET  网络通信，Windows AF_INET
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # SOCK_DGRAM表示UDP，SOCKET_STREAM表示TCP
+udp.connect(("192.168.1.2", 2425))  # connect函数的参数是一个元组，括号不能少(2425)是飞Q的端口号
+udp.send(mystr.encode("gbk"))  # 报文是以二进制的形式发送的
+udp.close()
